bd0f7cb76081087a2a960c981ff386d9b405851b
Nrodina <54550472+Nrodina@users.noreply.github.com>
2020-01-10 12:37:18 +0200

    ver89

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 app = Flask('app')
 @app.route('/')
 
 def index_page():
   return render_template("index.html")
-@app.route('/helth')
-def helth():
+@app.route('/health')
+def health():
   return "OK"
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
