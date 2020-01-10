9c67c8c94a4c86eea4e542363c4d94634a04c56e
Nrodina <54550472+Nrodina@users.noreply.github.com>
2020-01-10 15:19:39 +0200

    aaa

--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,18 @@
-console.log("Sveicinat no konsoles");
+const ATJAUNOT = 5000;
+
+async function lasiChatu() {
+    const atbilde = await fetch('/chats/lasi');
+    const datuObjektu = await atbilde.json();
+    raadiChatuVienkarsi(datuObjekts);
+}
+
+function raadiChatuVienkarsi(dati);
+    const jaunaRinda = "</br>";
+    let chats = "";
+    let chataDiv = document.getElementById("chats");
+    for(let rinda of dati ['chats']){
+        chats = chats + rinda + jaunaRinda;
+
+    }
+
+    chataDiv.innerHTML= chats;
