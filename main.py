985fc637df3b9f416946a1083617cb2362077ead
Nrodina <54550472+Nrodina@users.noreply.github.com>
2020-01-10 12:32:42 +0200

    ver17

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_templates
 app = Flask('app')
 @app.route('/')
+
 def index_page():
   return render_template("index.html")
 @app.route('/helth')
