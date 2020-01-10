257d5d6fa0940c9a917832d45bd76315b88602f0
Nrodina <54550472+Nrodina@users.noreply.github.com>
2020-01-10 12:25:31 +0200

    ver15

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask
-
-
+rom flask import Flask, render_template
 app = Flask('app')
-
-
 @app.route('/')
 def index_page():
-  return render_template(index)
-
-
+  return render_template("index.html")
+@app.route('/helth')
+def helth():
+  return "OK"
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
